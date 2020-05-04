fdb7051c5e89b64eb3570dfead7249f171667f0c
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2020-05-04 15:22:51 +0200

    Created view to add a group

---
+++ resources/views/addGroup.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Add new Group</div>
+                <div class="card-body">
+                    <form method="post" action="/group">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control">
+                        </div>
+                         <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea name="description" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Add Group</button>
+                        </div>
+                        {{ csrf_field() }}
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
