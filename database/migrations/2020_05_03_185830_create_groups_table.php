eea07b98164a0ee2813e870b6aa38ecedfdea771
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2020-05-04 11:51:43 +0200

    Created table columns for groups table

--- database/migrations/2020_05_03_185830_create_groups_table.php
+++ database/migrations/2020_05_03_185830_create_groups_table.php
@@ -15,6 +15,8 @@ class CreateGroupsTable extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
