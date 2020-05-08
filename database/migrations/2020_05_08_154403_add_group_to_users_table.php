bcdf9d7280b90231970af2db3a39fb7af8b05618
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2020-05-08 17:49:53 +0200

    Add new column 'group' to users table

--- database/migrations/2020_05_08_154403_add_group_to_users_table.php
+++ database/migrations/2020_05_08_154403_add_group_to_users_table.php
@@ -14,7 +14,7 @@ class AddGroupToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->integer('group')->unsigned();
         });
     }
 
@@ -26,7 +26,7 @@ class AddGroupToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['group']);
         });
     }
 }
