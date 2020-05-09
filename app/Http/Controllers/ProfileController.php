a440a93276225bf5bfef03837fda58d5e7ee16c8
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2020-05-09 17:37:11 +0200

    Show login details on profile page of user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class ProfileController extends Controller
 {
@@ -14,6 +15,7 @@ class ProfileController extends Controller
     
     public function show()
     {
-    	return view('profile');
+    	$user = Auth::user();
+    	return view('profile', compact('user'));
     }
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,6 +6,20 @@
             <div class="card">
                 <div class="card-header">Profile</div>
                 <div class="card-body">
+                	<form method="post" action="/profile/{{ $user->id }}">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" value="{{ $user->name }}" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="text" name="email" value="{{ $user->email }}" class="form-control">
+                        </div>
+                        <div class="form-group mt-4">
+                            <button type="submit" class="btn btn-primary">Change Profile</button>
+                        </div>
+                        {{ csrf_field() }}
+                    </form>
                 </div>
             </div>
         </div>
