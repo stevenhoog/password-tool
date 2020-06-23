143c6883b22fe2eb602d52a91d3c07c3d44fd246
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2020-06-23 11:06:52 +0200

    Installed Google Tag Manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,14 @@
 
     <title>{{ config('app.name') }}</title>
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NKNC9QL');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <!-- App.js loads jQuery v3.5.0 -->
@@ -60,6 +68,10 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKNC9QL"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
